66977525fa9ac55f04a6550e1675e497afba6246
engajaprireis <aboutlauanap@gmail.com>
2026-02-27 01:35:18 -0300

    Update contador.js

--- contador.js
+++ contador.js
@@ -1,1 +1,15 @@
+let doacoes = 1; // ALTERE SOMENTE AQUI
+let meta = 350;
 
+function atualizar() {
+  const contador = document.getElementById("contador");
+  const barra = document.getElementById("barra");
+
+  if (contador && barra) {
+    contador.innerText = doacoes + " / " + meta;
+    let porcentagem = (doacoes / meta) * 100;
+    barra.style.width = porcentagem + "%";
+  }
+}
+
+atualizar();
